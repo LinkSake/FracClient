34da23b8f7a828b4a1013fc6fbadb94fee76e50f
Luis Angel Ortega <angel-ortega12@hotmail.com>
2019-12-07 12:27:59 -0700

    Landing was added

---
+++ src/views/Landing.vue
@@ -0,0 +1,52 @@
+<template>
+   <div class="landing">
+    <sui-grid 
+    :columns="3">
+        <sui-grid-row 
+        stretched
+        class="landing-row"
+        verticalAlign="middle"> 
+            <sui-grid-column />
+            <sui-grid-column>
+                <h1 
+                is="sui-header" 
+                icon="home">
+                    FracApp
+                    <sui-header-subheader>
+                        Maneja tu fraccionamiento de una menera sencilla<span>™️</span>
+                    </sui-header-subheader>
+                </h1>
+                <div id="nav">
+                    <router-link 
+                    to="/">
+                        Hi
+                    </router-link> 
+                    |
+                    <router-link 
+                    to="/login">
+                        Login
+                    </router-link> 
+                    | 
+                    <router-link 
+                    to="/signup">
+                        Signup
+                    </router-link>
+                </div>
+            </sui-grid-column>
+            <sui-grid-column />
+        </sui-grid-row>
+    </sui-grid>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing'
+}
+</script>
+
+<style lang="css">
+    .landing-row{
+        height: 100vh;
+    }
+</style>
